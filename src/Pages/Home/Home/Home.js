968ae587c801441685048ccacffd097543830d4d
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-17 02:51:35 +0600

    fake data added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,19 +1,30 @@
 import React from "react";
+import Services from "../Services/Services";
 import "./Home.css";
 
 const Home = () => {
     return (
-        <div className="banner">
-            <div className="container">
-                <div className="banner-text-container d-flex justify-content-start align-items-center">
-                    <div>
-                        <h1>KEEP YOUR BODY FIT</h1>
-                        <h1>AND <span className="theme-text">STRONG</span></h1>
-                        <h3 className="text-light">With Steven AKA the <span className="theme-text">GYM FREAK</span></h3>
-                        <button className="contact-btn">Contact Now</button>
+        <div>
+            <div className="banner">
+                <div className="container">
+                    <div className="banner-text-container d-flex justify-content-start align-items-center">
+                        <div>
+                            <h1>KEEP YOUR BODY FIT</h1>
+                            <h1>
+                                AND <span className="theme-text">STRONG</span>
+                            </h1>
+                            <h3 className="text-light">
+                                With Steven AKA the{" "}
+                                <span className="theme-text">GYM FREAK</span>
+                            </h3>
+                            <button className="contact-btn">
+                                LET'S TRAIN NOW
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Services></Services>
         </div>
     );
 };
