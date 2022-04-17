41ad4fbd4f8bf98d80b5adfddcbecea84aa9b6f0
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-17 16:10:31 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Pages/Shared/Header/Header";
 import Home from "./Pages/Home/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./Pages/Shared/Footer/Footer";
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home></Home>}></Route>
                 <Route path="/home" element={<Home></Home>}></Route>
+                <Route path="*" element={<NotFound></NotFound>}></Route>
             </Routes>
             <Footer></Footer>
         </div>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NotFound.css';
+import notFound from '../../images/404.jpg'
+
+const NotFound = () => {
+    return (
+        <div className='not-found'>
+            <img className='img-fluid' src={notFound} alt="" />
+            <div>
+                <h2>404 Page Not Found</h2>
+                <h3>The Page you are looking for is unavailable</h3>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
