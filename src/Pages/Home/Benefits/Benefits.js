5535e966cd280108fe78f2ce3dba793c55ebec2d
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-04-18 18:16:14 +0600

    benefits section added

--- src/Pages/Home/Benefits/Benefits.js
+++ src/Pages/Home/Benefits/Benefits.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import './Benefits.css';
-import team from '../../../images/team.jpg';
-import equipment from '../../../images/equipment.jpg';
+import React from "react";
+import "./Benefits.css";
+import team from "../../../images/team.jpg";
+import equipment from "../../../images/equipment.jpg";
 
 const Benefits = () => {
     return (
-        <div className='benefits-section'>
-            <h1 className='benefit-title'>FRESH AND FUN WORKOUTS</h1>
-            <div className='benefits'>
-                <div className='bg-light benefit-card'>
-                    <img className='img-fluid' src={team} alt="" />
-                    <h2 className='text-dark'>HIGHLY MOTIVATING</h2>
-                    <p className='lead text-center text-dark'>Start your healthy lifestyle today</p>
+        <div className="benefits-section">
+            <h1 className="benefit-title">FRESH AND FUN WORKOUTS</h1>
+            <div className="benefits">
+                <div className="benefit-card-team">
+                    <img className="img-fluid" src={team} alt="" />
+                    <div>
+                        <h2>HIGHLY MOTIVATING</h2>
+                        <p className="lead text-center">
+                            Start your healthy lifestyle today
+                        </p>
+                    </div>
                 </div>
-                <div className='bg-light benefit-card'>
-                    <img className='img-fluid' src={equipment} alt="" />
-                    <h2 className='text-dark'>NEW GYM EXPERIENCE</h2>
-                    <p className='lead text-center text-dark'>Try your first training for free</p>
+                <div className="benefit-card-gym">
+                    <div className="free">
+                        <h2>NEW GYM EXPERIENCE</h2>
+                        <p className="lead text-center">
+                            Try your first training for free
+                        </p>
+                    </div>
+                    <img className="img-fluid" src={equipment} alt="" />
                 </div>
             </div>
         </div>
